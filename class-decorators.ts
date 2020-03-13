f07160171251cb1e2317b1ec44a12accd3f3b4a9
渡辺 孝之 <t.watanabe@ifcreate.co.jp>
2020-03-13 11:52:46 +0900

    deco

---
+++ class-decorators.ts
@@ -0,0 +1,17 @@
+@sealed
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+function sealed(constructor: Function) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+}
+
+console.log(new Greeter("company").greet());
