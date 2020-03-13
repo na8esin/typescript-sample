3d3ef132365ff21d6ce92b2315d0d67c88f86b96
渡辺 孝之 <t.watanabe@ifcreate.co.jp>
2020-03-13 13:33:17 +0900

    constructor

--- class-decorators.ts
+++ class-decorators.ts
@@ -9,9 +9,33 @@ class Greeter {
     }
 }
 
+class Creature {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
 function sealed(constructor: Function) {
     Object.seal(constructor);
     Object.seal(constructor.prototype);
 }
 
-console.log(new Greeter("company").greet());
+const greeter = new Greeter("company");
+
+console.log(greeter.greet());
+
+delete greeter.constructor
+console.log(greeter.greet());
+
+'use strict'
+delete Greeter.constructor;
+
+new Greeter('bello');
+
+delete Creature.constructor;
+const cr = new Creature('aaa');
+cr.greet();
