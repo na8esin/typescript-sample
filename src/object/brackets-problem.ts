993d3e7c84a75f3d23da089bd73edc9e613c58d2
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-30 09:36:00 +0900

    ブラケット参照を対応する

--- src/object/brackets-problem.ts
+++ src/object/brackets-problem.ts
@@ -1,15 +1,49 @@
-class DTO {
+import { strict as assert } from 'assert';
+
+class DTOHasAnyType {
+    // これを追加すればエラーは起きないが、
+    // いろいろな事情で追加できないorしたくないこともある
     [key: string]: any
     name?: string;
+    title?: string;
 }
 
-function mergeDtoWithFirestoreRecord(dto: DTO, firestoreObject: any) {
+class DTO {
+    name?: string;
+    title?: string;
+}
+
+function mergeDtoWithFirestoreRecord(dto: DTOHasAnyType, firestoreObject: any) {
     Object.keys(firestoreObject).map((key: string) => {
+        // DTOクラスに何も対策しないと下記のエラーになる
         // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'DTO'.
-        //No index signature with a parameter of type 'string' was found on type 'DTO'
+        // No index signature with a parameter of type 'string' was found on type 'DTO'
+        // なので対策したDTOHasAnyTypeを使う
         if (dto[key] === undefined) {
             dto[key] = firestoreObject[key];
         }
     });
 }
+
+// リフレクションで解決してみる
+function reflectiton(dto: DTO, firestoreObject: any) {
+    Object.keys(firestoreObject).map((key: string) => {
+        const name = Reflect.get(dto, key);
+        if (name === undefined) {
+            Reflect.set(dto, key, firestoreObject[key]);
+        }
+    });
+}
+
+const firestoreObject = { name: 'update' };
+
+const dto1 = {};
+mergeDtoWithFirestoreRecord(dto1, firestoreObject);
+assert.deepEqual(dto1, { name: 'update' });
+
+const dto2 = {};
+reflectiton(dto2, firestoreObject);
+assert.deepEqual(dto2, { name: 'update' });
+
+// 同じプロジェクトで変数などが被らないおまじない
 export { };
